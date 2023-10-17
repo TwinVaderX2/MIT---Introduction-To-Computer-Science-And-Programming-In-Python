a940cd815542c7734b521c86c780faa9bbf7224a
Phillip van Staden <phillipvanstaden@outlook.com>
2023-10-17 16:05:26 +0200

    fix!: removed duplicate code and imported from ps4b.py

--- Problem Sets/ps4/src/app/ps4c.py
+++ Problem Sets/ps4/src/app/ps4c.py
@@ -1,57 +1,15 @@
 # Problem Set 4C
-# Name: <your name here>
+# Name: Phillip van Staden
 # Collaborators:
-# Time Spent: x:xx
 
 import string
 from ps4a import get_permutations
+from ps4b import load_words, is_word
+import os
 
 ### HELPER CODE ###
-def load_words(file_name):
-    '''
-    file_name (string): the name of the file containing 
-    the list of words to load    
-    
-    Returns: a list of valid words. Words are strings of lowercase letters.
-    
-    Depending on the size of the word list, this function may
-    take a while to finish.
-    '''
-    
-    print("Loading word list from file...")
-    # inFile: file
-    inFile = open(file_name, 'r')
-    # wordlist: list of strings
-    wordlist = []
-    for line in inFile:
-        wordlist.extend([word.lower() for word in line.split(' ')])
-    print("  ", len(wordlist), "words loaded.")
-    return wordlist
-
-def is_word(word_list, word):
-    '''
-    Determines if word is a valid word, ignoring
-    capitalization and punctuation
-
-    word_list (list): list of words in the dictionary.
-    word (string): a possible word.
-    
-    Returns: True if word is in word_list, False otherwise
-
-    Example:
-    >>> is_word(word_list, 'bat') returns
-    True
-    >>> is_word(word_list, 'asdf') returns
-    False
-    '''
-    word = word.lower()
-    word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
-    return word in word_list
-
-
+# removed helper code and imported from ps4b
 ### END HELPER CODE ###
-
-WORDLIST_FILENAME = 'words.txt'
 
 # you may find these constants helpful
 VOWELS_LOWER = 'aeiou'
